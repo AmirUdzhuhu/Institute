cd12979842380a20120b9eb98b146f03a2ae2bd3
AmirUdzhuhu <amir_udzhuhu@mail.ru>
2021-05-16 00:19:03 +0300

    16.05.2021

---
+++ ЛЗ 6.1/script.js
@@ -0,0 +1,29 @@
+// Задание 1
+alert ('Я изучаю JavaScript');
+
+// Задание 2
+let a = 'Я ';
+let b = 'изучаю ';
+let c = 'JavaScript';
+alert (a+b+c);
+
+// Задание 3
+var boolean = true;
+var number = 777;
+var string = ' слово';
+alert (boolean);
+alert (number + string);
+
+// Задание 4
+var name = 'Курсант'
+var admin = name;
+alert (admin);
+
+// Задание 5
+var d, e, f;
+d = 'Я учусь в ';
+e = '36 ';
+f = 'учебной группе';
+alert (d+e+f);
+var z = d + e + f;
+alert (z);
