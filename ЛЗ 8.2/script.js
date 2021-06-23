b3b860c50ca31e63716167f7fdb38503e766c8f6
AmirUdzhuhu <amir_udzhuhu@mail.ru>
2021-06-23 22:00:44 +0300

    23.06.2021

---
+++ ЛЗ 8.2/script.js
@@ -0,0 +1,15 @@
+// Задание 1
+$('img').animate({width: '+=50%'}, 3000); // время в мс
+
+
+// Задание 2
+$('img').animate({opacity: 0.1, marginLeft: '+=50', height: '+=50'}, 2000);
+
+// Задание 3
+
+/*
+$('img').animate({width: '+=50%'}, 3000);
+$('img').animate({opacity: 0}, 2000);
+$('img').animate({width: '-=50%'}, 1000);
+$('img').animate({opacity: 1}, 2000);
+*/
